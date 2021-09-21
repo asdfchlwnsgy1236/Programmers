ec3d4be0e32a23fb0128d1060f106c65eebbd79a
Joonhyo Choi <asdfchlwnsgy1236@gmail.com>
2021-09-22 02:01:22 +0900

    Added the third problem.
    The problem of new ID recommendation (26/26 tests).

---
+++ NewIDRecommendation/NewIDRecommendation.cpp
@@ -0,0 +1,43 @@
+// URL: https://programmers.co.kr/learn/courses/30/lessons/72410
+
+#include <cctype>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <utility>
+// Note: The header below are for testing without using the site.
+#include <vector>
+
+std::string solution(std::string new_id){
+	std::string answer;
+	char period = '.';
+	for(int a = 0; a < new_id.size() && answer.size() < 15; ++a){
+		if(std::isalnum(new_id[a]) || std::strchr("-_.", new_id[a])){
+			if(new_id[a] == period && (answer.empty() || answer.back() == period)){
+				continue;
+			}
+			answer.append(1, std::tolower(new_id[a]));
+		}
+	}
+	if(answer.size() && answer.back() == period){
+		answer.pop_back();
+	}
+	if(answer.empty()){
+		answer.append(3, 'a');
+	}
+	else if(answer.size() < 3){
+		answer.append(3 - answer.size(), answer[answer.size() - 1]);
+	}
+
+	return answer;
+}
+
+// Note: Everything below this line is for testing without using the site.
+int main(){
+	std::vector<std::string> problems{"...!@BaT#*..y.abcdefghijklm", "z-+.^.", "=.=", "123_.def", "abcdefghijklmn.p"};
+	for(auto const &problem : problems){
+		std::cout << problem << " -> " << solution(problem) << '\n';
+	}
+
+	return 0;
+}
